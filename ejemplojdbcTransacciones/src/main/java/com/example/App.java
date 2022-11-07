35437a01a767033b1ea1892e9c5d95e5ebefdb3e
Sergio Poveda <114589487+Sergiok98@users.noreply.github.com>
2022-11-07 18:07:04 +0100

    1

--- ejemplojdbcTransacciones/src/main/java/com/example/App.java
+++ ejemplojdbcTransacciones/src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,32 +10,20 @@ import java.util.Scanner;
 
 public class App {
 
-    public final static String CADENA_CONEXION = "jdbc:mariadb://localhost:3306/product-manager";
+    public final static String CADENA_CONEXION = "jdbc:mariadb://localhost:3306/banco";
     public final static String USER = "root";
     public final static String PASS = "";
 
     /**
      * Mostrar productos con la referencia que indique el usuarios
      */
-    public static void apartado1() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("referencia del producto");
-        String referencia = sc.nextLine();
-        String sql = "SELECT * FROM product WHERE reference = '" + referencia + "'";
-
-        try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String ref = rs.getString("reference");
-                String name = rs.getString("name");
-                int precio = rs.getInt("price");
-                System.out.println(id + " - " + ref + " - " + name + " - " + precio);
+    public static void apartado1() {    
+            try (Connection conn = DriverManager.getConnection(CADENA_CONEXION, USER, PASS)) {
+                PreparedStatement stOrigin = conn.prepareStatement("UPDATE cuenta SET saldo = saldo - ? WHERE numero = ?");
+                
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
-        }
     }
 
     public static void main(String[] args) {
